9de4c45c623c57501d3f492acd7a74017676f916
harismalik603 <66439745+harismalik603@users.noreply.github.com>
2021-06-09 00:18:19 +0500

    add more files

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+var x = document.getElementById("one1");
+var y = document.getElementById("two2");
+var z = document.getElementById("three3");
+
+function one() {
 
 
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+    }
+}
 
+function two() {
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        x.style.display = "none";
+        z.style.display = "none";
+    }
+}
+
+function three() {
+    if (z.style.display === "none") {
+        z.style.display = "block";
+        x.style.display = "none";
+        y.style.display = "none";
+    } else {
+        z.style.display = "none";
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+}
